561708015c0e8dd5edbf1d0a4e1a2e7e46773efc
Ahmed Anber <begnulinux@gmail.com>
2019-08-16 23:37:21 +0200

    Get controller name from url and uppercase first character and shift controller from url variables

--- core/Router.php
+++ core/Router.php
@@ -3,7 +3,10 @@
 class Router {
 
   public static function route($url) {
-    var_dump($url);
-    die();
+    
+    //controller
+    $controller = (isset($url[0]) && $url[0] != '') ? ucwords($url[0]) : DEFAULT_CONTROLLER ;
+    array_shift($url);
+    dnd($url);
   }
 }
